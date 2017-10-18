bf97fde29e0978ebdc40b81972a196d938368338
nori007 <nori007@naver.com>
2017-10-18 13:08:02 +0900

    book-component-class

--- app/book.component.ts
+++ app/book.component.ts
@@ -2,7 +2,22 @@ import {Component} from '@angular/core';
 
 @Component({
     selector: 'my-book',
-    template: '<h3>BookComponent</h3>'
+    template: `
+        <h1>Book</h1>
+        <p>{{name}}</p>
+        <p>{{getName()}}</p>
+        <p>{{setName('하이브리드 앱')}}</p>
+        `
 })
 
-export class BookComponent{}
+export class BookComponent{
+    name="안드로이드 게임 프로그래밍"
+
+    setName(name){
+        this.name = name;
+    }
+
+    getName(){
+        return this.name;
+    }
+}
