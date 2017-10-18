67aab43311e4563c15d7dfaf98c898892341d5d5
nori007 <nori007@naver.com>
2017-10-18 14:20:48 +0900

    book-bind-event

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>AppComponent</h1><my-book></my-book>'
+    template: '<my-book></my-book>'
 })
 
 export class AppComponent {}
